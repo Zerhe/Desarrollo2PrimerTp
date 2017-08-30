cbe2d2a04fbd4e40895fc9b815f70e4bd1837ce8
Wemir <dante_ro@hotmail.com.ar>
2017-08-29 21:46:59 -0300

    Primer Prototipo

--- Desarrollo2Tp/Assets/Scripts/Respawn.cs
+++ Desarrollo2Tp/Assets/Scripts/Respawn.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Respawn : MonoBehaviour {
+    private Vector3 positionToRespawn;
+    private Rigidbody rgb;
 
-	// Use this for initialization
 	void Start () {
-		
+        rgb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+		if (transform.position.y < -15)
+        {
+            positionToRespawn.x = Random.Range(-40, 40);
+            positionToRespawn.y = 25;
+            positionToRespawn.z = Random.Range(-40, 40);
+            transform.position = positionToRespawn;
+            //rgb.velocity = Vector3.zero;
+            rgb.angularVelocity = Vector3.zero;
+        }
 	}
 }
