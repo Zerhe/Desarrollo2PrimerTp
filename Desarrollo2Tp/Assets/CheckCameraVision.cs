ada1d24e88c187d4f9921805b630a6837d79f337
Zerhe <dante_ro@hotmail.com.ar>
2020-02-18 11:39:10 -0300

    Los objetos ya no obstaculizan la camara.

---
+++ Desarrollo2Tp/Assets/CheckCameraVision.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckCameraVision : MonoBehaviour {
+
+    [SerializeField]
+    private float velMov;
+    private bool checkCamera = false;
+    [SerializeField]
+    private Transform playerT;
+    private float distanceMax;
+    private float distanceActual;
+    private Vector3 position01;
+    private Vector3 position02;
+    private bool playerMoving = false;
+    private Rigidbody playerRgb;
+
+    private void Awake()
+    {
+        playerRgb = playerT.gameObject.GetComponent<Rigidbody>();
+    }
+    void Start ()
+    {
+        distanceActual = distanceMax = Vector3.Distance(playerT.position, transform.position);
+        position02 = position01 = playerT.position;
+    }
+	
+	void Update ()
+    {
+        print(playerMoving);
+        //print(playerRgb.velocity.magnitude);
+
+        if (playerRgb.velocity.magnitude > 1 )
+        {
+            playerMoving = true;
+        }
+        else
+            playerMoving = false;
+
+        distanceActual = Vector3.Distance(playerT.position, transform.position);
+
+        if (checkCamera == true && distanceActual > 1.5)
+        {
+            transform.Translate(Vector3.forward * velMov);
+        }
+        else if( checkCamera == false && distanceActual < distanceMax && playerMoving)
+        {
+            transform.Translate(Vector3.forward * -velMov);
+        }
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            checkCamera = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 9)
+        {
+            checkCamera = false;
+        }
+    }
+}
